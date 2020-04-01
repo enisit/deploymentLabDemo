632307ccf1a085a07ee4d0096f650db082582061
Enis <enis.gegic@gmail.com>
2020-04-01 19:42:14 +0200

    Homepage changes depoyed automaticly

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel wen application
+                    Laravel web application deplyed with Heroku and TravisCI
                 </div>
 
                 <div class="links">
